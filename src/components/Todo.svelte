31149fc742cb005f23b42013dc295baf4602261d
sinaniya <sinaniya@gmail.com>
2022-07-21 13:21:24 +0430

    add todo item view

---
+++ src/components/Todo.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let todo;
+	import { deleteTodo, toggleTodo } from '../stores/todoStore.js';
+</script>
+
+<li class="bg-white flex items-center shadow-sm border border-gray-200 rounded-lg my-2 py-2 px-4">
+	<input
+		class="mr-2 form-checkbox h-5 w-5"
+		name="Completed"
+		type="checkbox"
+		checked={todo.done}
+		on:change={() => toggleTodo(todo.id)}
+	/>
+	<span class={`flex-1 text-gray-800 ${todo.done ? 'line-through' : ''}`}>{todo.text}</span>
+	<button
+		class="text-sm bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+		type="button"
+		on:click={() => deleteTodo(todo.id)}
+	>
+		Delete
+	</button>
+</li>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,13 @@
 <script>
 	import TodoForm from '../components/TodoForm.svelte';
+	import Todo from '../components/Todo.svelte';
+	import { todos } from '../stores/todoStore.js';
 </script>
 
 <main>
 	<h1 class="text font-bold text-center text-gray-800 md:text-3xl">My Todos</h1>
 	<TodoForm />
+	{#each $todos as todo}
+		<Todo {todo} index={todo.id} />
+	{/each}
 </main>
